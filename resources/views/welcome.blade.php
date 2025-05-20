1ecc8782b21561d1756545bcc22ee9c04aeee8f8
Boat <boat@example.com>
2025-05-20 22:20:26 +0700

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,7 +181,6 @@
 
         </style>
 </head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-...." crossorigin="anonymous">
 
 <body>
 
